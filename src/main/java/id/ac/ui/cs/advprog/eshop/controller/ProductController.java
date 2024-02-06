0bfb76009a23d2ffe2d4831daa51c6b7e2fa1c58
Matthew Hotmaraja Johan Turnip <matthewhjt72@gmail.com>
2024-02-07 01:27:13 +0700

    Added GetMapping and PostMapping methods for edit product feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/product")
@@ -33,5 +34,18 @@ public class ProductController {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
         return "productList";
+    }
+
+    @GetMapping("/edit/{productId}")
+    public String editProductPage(@PathVariable UUID productId, Model model) {
+        Product product = service.findProductById(productId);
+        model.addAttribute("product", product);
+        return "editProduct";
+    }
+
+    @PostMapping("/edit/{productId}")
+    public String editProduct(@ModelAttribute Product product, Model model) {
+        service.editProduct(product);
+        return "redirect:/product/list";
     }
 }
